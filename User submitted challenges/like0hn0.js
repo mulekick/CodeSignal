a7684b448a9646334559424930e8efe2f2507f52
mulekick-local <karim_laouari@hotmail.com>
2018-04-25 14:43:41 +0200

    Updating user submitted challenges

--- User submitted challenges/like0hn0.js
+++ User submitted challenges/like0hn0.js
@@ -1,6 +1,5 @@
 /*
 like0hn0 = grid => {	
-	const dir = ["U","D","L","R"];
 	const mvs = {U:1,D:1,L:1,R:1};
 	const val = (x, y) => !grid[y] || !grid[y][x] ? - 1 : 1; 
 	const mov = {
@@ -10,20 +9,20 @@ like0hn0 = grid => {
 		R:(x, y, m) => val(x + m, y)
 	};
 	const chk = function(x, y) {
+		let sum = 0;
         let m = Object.assign({}, mvs);
-		dir.forEach(d => {
-			let t = mov[d](x, y, m[d]);
-			while(t > 0) t = mov[d](x, y, ++m[d]);
-			--m[d];
-		});
-		return grid[y][x] === dir.reduce((r, z) => r += m[z], 0);
+		for (dir in m) {
+			let t = mov[dir](x, y, m[dir]);
+			while(t > 0) t = mov[dir](x, y, ++m[dir]);
+			sum += --m[dir];
+		}
+		return grid[y][x] === sum;
 	}
     return grid.every((c, y) => c.every((v, x) => v === 0 ? true : chk(x, y)));
 }
 */
 //SEE ABOVE FOR CLEAN CODE AND BELOW FOR GARBAGE
 like0hn0 = g => {	
-	d = ["U","D","L","R"]
 	e = {U:1,D:1,L:1,R:1}
 	f = (x, y) => !g[y] || !g[y][x] ? - 1 : 1
 	o = {
@@ -33,13 +32,14 @@ like0hn0 = g => {
 		R:(x, y, m) => f(x + m, y)
 	}
 	h = function(x, y) {
+        s = 0;
         m = Object.assign({}, e)
-		d.forEach(a => {
+		for (a in m) {
 			t = o[a](x, y, m[a])
 			while(t > 0) t = o[a](x, y, ++m[a])
-			--m[a]
-		})
-		return g[y][x] == d.reduce((r, z) => r += m[z], 0)
+			s += --m[a]
+		}
+		return g[y][x] == s
 	}
     return g.every((c, y) => c.every((v, x) => v == 0 ? 1 : h(x, y)))
 }
