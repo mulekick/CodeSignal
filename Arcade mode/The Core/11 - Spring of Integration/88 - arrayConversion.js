e1798f006a4cfb284dc609aeb62c14bae0ea0cbc
mulekick-local <karim_laouari@hotmail.com>
2018-05-18 23:38:11 +0200

    Updating arcade mode

---
+++ Arcade mode/The Core/11 - Spring of Integration/88 - arrayConversion.js
@@ -0,0 +1,12 @@
+'use strict'
+arrayConversion = a => {
+	var r, t, i = 0;
+	if (a.length === 1) return a[0];
+	do {
+		r = [];
+		for (let j = 0; j < a.length; j += 2) r.push(i ? a[j] * a[j + 1] : a[j] + a[j + 1]);
+		a = r.slice();
+		i ^= 1;
+	} while (a.length > 1)
+    return a[0];
+}
