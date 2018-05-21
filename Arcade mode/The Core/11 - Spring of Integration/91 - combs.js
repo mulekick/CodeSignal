39537ef09c5f58e157f826bd99e9a70cdbfe9342
mulekick-local <karim_laouari@hotmail.com>
2018-05-21 16:13:23 +0200

    Updating arcade mode

--- Arcade mode/The Core/11 - Spring of Integration/91 - combs.js
+++ Arcade mode/The Core/11 - Spring of Integration/91 - combs.js
@@ -1,3 +1,53 @@
+//==================== BITWISE SOLUTION ===================
+'use strict' 
+combs = (c1, c2) => {
+	var l1, l2, r = [];
+	const t = x => parseInt(x.replace(/[*]/g, "1").replace(/[.]/g, "0"), 2), u = x => Math.floor(Math.log2(x)) + 1;
+	c1 = t(c1), c2 = t(c2), l1 = u(c1), l2 = u(c2);
+	const o1 = l1, o2 = l2;	
+	c1 <<= (l1 + l2), c2 <<= l1, l1 = u(c1), l2 = u(c2);
+	for (let n = 0; n <= o1 + o2; n++) {
+		let k = Math.max(l1 - n, l2), m = Math.min(l2 - n, o1);
+		if ((c1 & c2) === 0) r.push(k - m);
+		c1 >>>= 1;
+	}
+	return Math.min(...r);
+}
+/*
+comb1: 	"*..*.*" 	=> 100101 = 37 => l1 = floor(log2(37)) + 1 = 6
+comb2: 	"*.***" 	=> 010111 = 23 => l2 = floor(log2(23)) + 1 = 5
+
+comb1 <<= l1 + l2
+comb2 <<= l1
+
+o1 = l1
+o2 = l2
+
+l1 = floor(log2(comb1)) + 1
+l2 = floor(log2(comb2)) + 1
+
+100101				l1 - n		l2 - n		n = 0 										
+      10111			l2			o1	 		k = max(l1 - n, l2), m = min(l2 - n, o1)
+											comb1 & comb2 === 0 ==> L = k - m, comb1 >>>= 1
+---------------------------------------------------------- 
+ 100101				l1 - n		l2 - n		n = 1
+      10111			l2			o1		 	k = max(l1 - n, l2), m = min(l2 - n, o1) + 1)
+----------------------------------------------------------
+  100101			l1 - n		l2 - n		n = 2 			
+      10111			l2			o1		 	k = max(l1 - n, l2), m = min(l2 - n, o1)
+											comb1 & comb2 === 0 ==> L = k - m, comb1 >>>= 1
+----------------------------------------------------------
+      100101		l1 - n		l2 - n		n = o1
+      10111			l2			o1		 	k = max(l1 - n, l2), m = min(l2 - n, o1)
+----------------------------------------------------------
+           100101	l1 - n		l2 - n		n = o1 + o2 	
+      10111			l2			o1		 	k = max(l1 - n, l2), m = min(l2 - n, o1)
+											comb1 & comb2 === 0 ==> L = k - m, comb1 >>>= 1
+----------------------------------------------------------    
+*/
+
+//==================== STRING COMPARE SOLUTION ===================
+/*
 'use strict'
 combs = (c1, c2) => {
 	const l1 = c1.length, l2 = c2.length,
@@ -15,7 +65,7 @@ combs = (c1, c2) => {
 	}
 	return Math.min(...r);
 }
-
+*/
 /*
 comb1: 	"*..*.*" 	=> l1 = 6
 comb2: 	"*.***" 	=> l2 = 5
