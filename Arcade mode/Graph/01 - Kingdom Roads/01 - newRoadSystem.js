32cb0e5b00c118283446d9b1b6d107799a51a435
mulekick <karim_laouari@hotmail.com>
2020-09-08 22:50:00 +0200

    Remove Graph

--- Arcade mode/Graph/01 - Kingdom Roads/01 - newRoadSystem.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-newRoadSystem = r => {
-	let c = r.map(x => x.reduce((r, x) => r += x ? 1 : 0, 0));
-	for (let i = 0; i < r.length; i++)
-		for (let j = 0; j < r[i].length; j++) r[i][j] ? --c[j] : false;
-	return c.every(x => x === 0);
-}
